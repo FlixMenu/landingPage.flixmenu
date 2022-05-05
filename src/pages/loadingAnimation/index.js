e9308523a026991c309a259c9857af64797aae92
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-05 23:49:56 +0100

    Created the loadingAnimation Page

---
+++ src/pages/loadingAnimation/index.js
@@ -0,0 +1,9 @@
+const LoadingAnimation = (props) => {
+    return (
+        <div className="w-screen h-screen bg-gradient-to-br from-[#FDA53F] to-[#FF174C]">
+
+        </div>
+    )
+}
+
+export default LoadingAnimation;
