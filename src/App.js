87aab9ffb2e3690d2b3829b67586887a830c8454
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-06 15:11:16 +0100

    Enabled page transition using framer motion

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import React from "react"
 import { Layout } from "./components"
 import "./index.css"
 import { DataProvider } from "./context/Datacontext"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom"
 import { Home } from "./pages"
 import LoadingAnimation from "./pages/loadingAnimation"
+import { AnimatePresence } from "framer-motion"
 
 
 export default function App() {
+  const location = useLocation()
   return (
     <DataProvider>
       <Layout>
         <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/loadingAnimation" element={<LoadingAnimation />} />
-          </Routes>
-        </Router>
+          <AnimatePresence exitBeforeEnter>
+            <Routes location={location} key={location.pathname}>
+              <Route path="/" element={<Home />} />
+              <Route path="/loadingAnimation" element={<LoadingAnimation />} />
+            </Routes>
+          </AnimatePresence>
+          </Router>
       </Layout>
     </DataProvider>
   )
